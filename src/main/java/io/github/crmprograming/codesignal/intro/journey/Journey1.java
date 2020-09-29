4f2aa19c4efc72b8f029cd61490a6f5a1fe2756b
César Ravelo Martínez <crmprograming@gmail.com>
2020-09-29 10:57:44 +0100

    Resuelto el ejercicio primero de Journey

---
+++ src/main/java/io/github/crmprograming/codesignal/intro/journey/Journey1.java
@@ -0,0 +1,9 @@
+package io.github.crmprograming.codesignal.intro.journey;
+
+public class Journey1 {
+	
+	public static int add(int param1, int param2) {
+	    return param1 + param2;
+	}
+
+}
--- src/main/java/io/github/crmprograming/codesignal/main/Main.java
+++ src/main/java/io/github/crmprograming/codesignal/main/Main.java
@@ -1,9 +1,11 @@
 package io.github.crmprograming.codesignal.main;
 
+import io.github.crmprograming.codesignal.intro.journey.Journey1;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(Journey1.add(2, 3));
 
 	}
 
