6f6adeaf6ecea0dc9438ac9901b44e77bf1972a7
César Ravelo Martínez <crmprograming@gmail.com>
2020-09-29 11:03:13 +0100

    Implementada la solución a Journey 2

---
+++ src/main/java/io/github/crmprograming/codesignal/intro/journey/Journey2.java
@@ -0,0 +1,8 @@
+package io.github.crmprograming.codesignal.intro.journey;
+
+public class Journey2 {
+	
+	public static int centuryFromYear(int year) {
+	    return (int) Math.ceil(year / 100.0);
+	}
+}
--- src/main/java/io/github/crmprograming/codesignal/main/Main.java
+++ src/main/java/io/github/crmprograming/codesignal/main/Main.java
@@ -1,12 +1,17 @@
 package io.github.crmprograming.codesignal.main;
 
 import io.github.crmprograming.codesignal.intro.journey.Journey1;
+import io.github.crmprograming.codesignal.intro.journey.Journey2;
 
 public class Main {
 
 	public static void main(String[] args) {
+		// Journey 1
 		System.out.println(Journey1.add(2, 3));
-
+		
+		// Journey 2		
+		System.out.println(Journey2.centuryFromYear(1905));
+		System.out.println(Journey2.centuryFromYear(1700));
 	}
 
 }
